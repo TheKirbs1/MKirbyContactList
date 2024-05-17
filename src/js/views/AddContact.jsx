59b9fe2c8bb9d7f2ebcb3c2905902bea69848fbc
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-05-17 00:09:47 +0000

    Styling added, Static form for adding contacts added

--- src/js/views/AddContact.jsx
+++ src/js/views/AddContact.jsx
@@ -3,12 +3,25 @@ import React from "react"
 const AddContact = () => {
 
     return (
-        <>
-            <div>
-                AddContact section.
+        <form>
+            <div class="form-group">
+                <label for="Full-Name">Full Name</label>
+                <input type="name" class="form-control" id="Full-Name" placeholder="Full Name"/>
             </div>
-        </>
-    );
+            <div class="form-group">
+                <label for="Email">Email address</label>
+                <input type="email" class="form-control" id="Email" placeholder="Enter email" />
+            </div>
+            <div class="form-group">
+                <label for="Phone-Number">Phone</label>
+                <input type="phone-number" class="form-control" id="Phone-Number" placeholder="Phone" />
+            </div>
+            <div class="form-group">
+                <label for="Address">Address</label>
+                <input type="address" class="form-control" id="Address" placeholder="Address" />
+            </div>
+        </form>
+    )
 }
 
 export default AddContact;
